b750658ee5da16ad004cd587d2445a8ff27a3cc7
Epskampie <simon@frontwise>
2012-11-22 16:15:08 +0100

    helper scripts

---
+++ permissions_fix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chown -R simon:simon *
+chmod -R 777 app/logs/
+chmod -R 777 app/cache/
+chmod -R 777 web/uploads/
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git pull
+./cache_clear.sh
+./permissions_fix.sh
