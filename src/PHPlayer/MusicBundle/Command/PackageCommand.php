4a8a81155102b4a3950b14d65b9a40acfd67a58c
Epskampie <simon@frontwise>
2012-11-26 15:05:51 +0100

    Removed swift dependency. Started package command

---
+++ src/PHPlayer/MusicBundle/Command/PackageCommand.php
@@ -0,0 +1,68 @@
+<?
+namespace PHPlayer\MusicBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use PHPlayer\MusicBundle\Helper\FileHelper;
+
+class PackageCommand extends ContainerAwareCommand
+{
+    private $output;
+
+    protected function configure()
+    {
+        $this
+            ->setName('phplayer:package')
+            ->setDescription('Remove some files and folders as to prepare a zipped distribution version')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->output = $output;
+
+        $output->writeln('Packaging.');
+
+        $root = realpath(dirname(__FILE__).'/../../../..');
+        $output->writeln($root);
+
+        if (basename($root) == 'PHPlayer') {
+            $output->writeln('Not operating on a copy of PHPlayer, stopping.');
+            return;
+        }
+
+        $output->writeln('Removing .git dirs');
+        $this->rmGit($root);
+        $output->writeln('');
+
+        // Clear some temp dirs
+        foreach (array('app/cache', 'app/logs', 'web/uploads') as $dir) {
+            foreach ( array_diff(scandir($dir), array('.','..')) as $file) {
+                if (is_file("$root/$dir/$file")) {
+                    unlink("$root/$dir/$file");
+                } else {
+                    $output->writeln("removing ". "$root/$dir/$file");
+                }
+            }
+        }
+    }
+
+
+    protected function rmGit($dir) {
+        $output = $this->output;
+
+        foreach ( array_diff(scandir($dir), array('.','..')) as $file) {
+            if (is_dir($dir.'/'.$file)) {
+                if ($file == '.git') {
+                    $output->writeln("removing: \t".$dir.'/'.$file);
+                    FileHelper::delTree($dir.'/'.$file);
+                } else {
+                    $this->rmGit($dir.'/'.$file);
+                }
+            }
+        }
+    }
+}
--- src/PHPlayer/MusicBundle/Helper/FileHelper.php
+++ src/PHPlayer/MusicBundle/Helper/FileHelper.php
@@ -98,4 +98,15 @@ class FileHelper
             mkdir($dir, 0777, true);
         }
     }
+
+    public static function delTree($dir) {
+    	if (!is_dir($dir)) return false;
+
+		$files = array_diff(scandir($dir), array('.','..'));
+		foreach ($files as $file) {
+			(is_dir("$dir/$file")) ? self::delTree("$dir/$file") : unlink("$dir/$file");
+		}
+		
+		return rmdir($dir);
+	}
 }
