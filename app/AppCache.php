0a867ed087ecb3609da8cc9a6631ffa9f8f63e04
Epskampie <simon@frontwise>
2013-01-18 23:41:37 +0100

    Started caching

--- app/AppCache.php
+++ app/AppCache.php
@@ -6,4 +6,16 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+	protected function getOptions()
+    {
+        return array(
+            'debug'                  => false,
+            'default_ttl'            => 0,
+            'private_headers'        => array('Authorization', 'Cookie'),
+            'allow_reload'           => true,
+            'allow_revalidate'       => true,
+            'stale_while_revalidate' => 30,
+            'stale_if_error'         => 60,
+        );
+    }
 }
--- web/app_dev.php
+++ web/app_dev.php
@@ -22,9 +22,11 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
